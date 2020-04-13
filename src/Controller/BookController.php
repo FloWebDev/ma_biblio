ca13c0640ac227c4eefdc82dc84bb8b875573171
FloWebDev <mathevon.florian@gmail.com>
2020-04-14 01:51:43 +0200

    book route

---
+++ src/Controller/BookController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class BookController extends AbstractController
+{
+    /**
+     * @Route("/book/{slug}", name="book", methods={"GET", "POST"})
+     * @ParamConverter("user", options={"mapping": {"slug": "slug"}})
+     */
+    public function index($slug, User $user)
+    {
+        return $this->render('book/index.html.twig', [
+            'user' => $user,
+            'slug' => $slug,
+        ]);
+    }
+}
