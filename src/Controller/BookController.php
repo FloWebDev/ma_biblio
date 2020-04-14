ba3e7b99baf1a66f85e86a2b2268676be1387841
fmathevon <fmathevon@cleonet.fr>
2020-04-14 16:22:28 +0200

    KnpPaginatorBundle

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -4,23 +4,34 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Repository\BookRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 class BookController extends AbstractController
 {
     /**
      * @Route("/book/{slug}", name="book", methods={"GET", "POST"})
      * @ParamConverter("user", options={"mapping": {"slug": "slug"}})
+     * 
+     * @link https://ourcodeworld.com/articles/read/593/using-a-bootstrap-4-pagination-control-layout-with-knppaginatorbundle-in-symfony-3
+     * @link https://github.com/KnpLabs/KnpPaginatorBundle
      */
-    public function index($slug, User $user, BookRepository $bookRepo)
+    public function index($slug, User $user, BookRepository $bookRepo, Request $request, PaginatorInterface $paginator)
     {
         $books = $bookRepo->findBy([
             'user' => $user->getId()
         ], [
             'title' => 'ASC'
         ]);
+
+        $books = $paginator->paginate(
+            $books,
+            $request->query->getInt('page', 1),
+            10
+        );
 
         return $this->render('book/index.html.twig', [
             'user' => $user,
