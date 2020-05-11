f33a6b6f20938491ade2af822941b90b192d65db
FloWebDev <mathevon.florian@gmail.com>
2020-05-11 15:26:42 +0200

    improve password visibility

--- public/assets/js/viewPassword.js
+++ public/assets/js/viewPassword.js
@@ -1,16 +1,55 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('#wrapper i').addEventListener('click', function(e) {
-        var inputPassword = document.querySelector('#inputPassword');
+    // Formulaire de connexion
+    if (document.querySelector('#wrapper i')) {
+        document.querySelector('#wrapper i').addEventListener('click', function(e) {
+            var inputPassword = document.querySelector('#inputPassword');
+    
+            // Gestion de l'affichage du mot de passe
+            if(inputPassword.type == 'password') {
+                inputPassword.type = 'text';
+                e.target.classList.remove('fa-eye');
+                e.target.classList.add('fa-eye-slash');
+            } else {
+                inputPassword.type = 'password';
+                e.target.classList.remove('fa-eye-slash');
+                e.target.classList.add('fa-eye');
+            }
+        });
+    }
 
-        // Gestion de l'affichage du mot de passe
-        if(inputPassword.type == 'password') {
-            inputPassword.type = 'text';
-            e.target.classList.remove('fa-eye');
-            e.target.classList.add('fa-eye-slash');
-        } else {
-            inputPassword.type = 'password';
-            e.target.classList.remove('fa-eye-slash');
-            e.target.classList.add('fa-eye');
-        }
-    });
+    // Formulaire d'inscription et modification
+    // Mot de passe
+    if (document.querySelector('#wrapper_first_password i')) {
+        document.querySelector('#wrapper_first_password i').addEventListener('click', function(e) {
+            var inputPassword = document.querySelector('#user_password_first');
+    
+            // Gestion de l'affichage du mot de passe
+            if(inputPassword.type == 'password') {
+                inputPassword.type = 'text';
+                e.target.classList.remove('fa-eye');
+                e.target.classList.add('fa-eye-slash');
+            } else {
+                inputPassword.type = 'password';
+                e.target.classList.remove('fa-eye-slash');
+                e.target.classList.add('fa-eye');
+            }
+        });
+    }
+    // Confirmation mot de passe
+    if (document.querySelector('#wrapper_second_password i')) {
+        document.querySelector('#wrapper_second_password i').addEventListener('click', function(e) {
+            var inputPassword = document.querySelector('#user_password_second');
+    
+            // Gestion de l'affichage du mot de passe
+            if(inputPassword.type == 'password') {
+                inputPassword.type = 'text';
+                e.target.classList.remove('fa-eye');
+                e.target.classList.add('fa-eye-slash');
+            } else {
+                inputPassword.type = 'password';
+                e.target.classList.remove('fa-eye-slash');
+                e.target.classList.add('fa-eye');
+            }
+        });
+    }
 });
